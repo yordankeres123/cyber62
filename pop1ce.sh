9ccba9b1136d850d8c2c809c322e7c4baadcbdcf
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-20 23:41:12 +0700

    Add files via upload

---
+++ pop1ce.sh
@@ -0,0 +1,19 @@
+clear
+
+echo "#############################"| lolcat
+figlet MEMBER| lolcat
+echo
+figlet OFICIAL| lolcat
+echo "+++++++++++++++++++++++++++++"| lolcat
+figlet POP1C3| lolcat
+echo
+figlet CYBER 62| lolcat
+echo
+figlet TEAM | lolcat
+echo "#############################"| lolcat
+echo
+echo "---------------------------------"
+figlet N6ENT*D LO!! | lolcat
+echo "---------------------------------"
+
+date | lolcat
